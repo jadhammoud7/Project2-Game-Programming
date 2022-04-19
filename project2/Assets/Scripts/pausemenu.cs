ad0c9f122e9cd70e2d3440015edaab083c2e74db
jadhammoud007 <91679985+jadhammoud007@users.noreply.github.com>
2022-04-19 04:05:06 +0300

    added resume pause

---
+++ project2/Assets/Scripts/pausemenu.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pausemenu : MonoBehaviour
+{
+    
+    public  static bool GamePaused=false;
+    public GameObject pausemenuUI;
+
+    // Update is called once per frame
+ void Start() {
+    pausemenuUI.SetActive(false);
+}
+
+     public void QuitGamebtn(){
+        Debug.Log("quited");
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(GamePaused){
+                Resume();
+            }else{
+                Pause();
+            }
+        } 
+    }
+    public void Resume(){
+        pausemenuUI.SetActive(false);
+        Debug.Log("i am playing now");
+        Time.timeScale=1f;
+        GamePaused=false;
+    }
+    void Pause(){
+        pausemenuUI.SetActive(true);
+        Debug.Log("i paused now");
+        Time.timeScale=0f;
+        GamePaused=true;
+    }
+
+}   
